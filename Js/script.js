c8c6282d0d78ecd24266908d3b082f03fce61553
sfprakash <prakash.sf4626@gmail.com>
2024-04-30 14:17:16 +0530

    Adding JavaScript files

---
+++ Js/script.js
@@ -0,0 +1,24 @@
+// var num = 4;
+// switch(num)
+// {
+//     case 1:
+//     {
+//         document.write("Function 1");
+//         break;
+//     }
+//     case 2:
+//     {
+//         document.write("Function 2");
+//         break;
+//     }
+//     case 3:
+//     {
+//         document.write("Function 3");
+//            break;
+//     }
+//     default :
+//     {
+//         document.write("Invalid Function");
+//         break;
+//     }
+// }
